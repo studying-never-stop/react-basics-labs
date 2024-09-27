0bceaedc8e43948159d740c611d88b5dd80979b3
Wu SongYu <wuwilliam2002@gmail.com>
2024-09-27 14:02:23 +0100

    Completed Tasky Lab 1(2)

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -6,13 +6,9 @@ function App() {
   return (
     <div className="container">
       <h1>tasky</h1>
-      <Task title='Dishes' deadline='Today' />
-      <Task title="Laundry" deadline="Tomorrow" />
-      <Task title="Tidy" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-      </Task>
-      
+      <Task title='Dishes' deadline='Today' description="Empty dashwasher"/>
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"/>
+      <Task title="Tidy" deadline="Today" />        
     </div>
   );
 }
