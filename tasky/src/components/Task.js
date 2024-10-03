5521acc8abf1faea0457176b28dda631eac0c15e
Wu SongYu <wuwilliam2002@gmail.com>
2024-10-03 12:53:19 +0100

    Delete a task

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -7,6 +7,7 @@ const Task = (props) => {
             <p>due: {props.deadline}</p>
             <p className="description">{props.description}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
     )
 }
